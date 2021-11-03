fdfe069fd80e6f4df36b08736c498c693a4ec37f
PranavSawant86 <92008025+PranavSawant86@users.noreply.github.com>
2021-11-03 13:00:51 +0530

    Update Style.cc

--- Style.cc
+++ Style.cc
@@ -1,1 +1,10 @@
-
+*{
+    margine: 0;
+    padding: 0;
+    box sizing: border-box;
+    font family: 'poppins', sans-serif;
+}
+.container{
+  width:100%;
+  height:100vh;
+  background-image: linear-gradient(rgba(0,0,0,0.3),transperant),url(
